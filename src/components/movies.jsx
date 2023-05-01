a6947f7b47bbe2afc6dd839d3f4010f5282a2638
arishorts <arieljschwartz@gmail.com>
2023-05-01 13:44:18 -0700

    refactored code to correctly abstrat pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./like";
 import Pagination from "./pagination";
 import { paginate } from "../utils/paginate";
 import Filter from "./filter";
 import { getGenres } from "../services/fakeGenreService";
+import MoviesTable from "./moviesTable";
 
 class Movies extends Component {
   state = {
@@ -78,19 +78,11 @@ class Movies extends Component {
           </div>
           <div className="col">
             <p>Showing {filtered.length} movies in the database</p>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Title</th>
-                  <th scope="col">Genre</th>
-                  <th scope="col">Stock</th>
-                  <th scope="col">Rate</th>
-                  <th />
-                  <th />
-                </tr>
-              </thead>
-              <tbody>{this.renderMovies(movies)}</tbody>
-            </table>
+            <MoviesTable
+              movies={movies}
+              onLike={this.handleLike}
+              onDelete={this.handleDelete}
+            />
             <Pagination
               numberPages={filtered.length}
               pageSize={pageSize}
@@ -101,29 +93,6 @@ class Movies extends Component {
         </div>
       </React.Fragment>
     );
-  }
-
-  renderMovies(movies) {
-    return movies.map((movie) => (
-      <tr key={movie._id}>
-        <td>{movie.title}</td>
-        <td>{movie.genre.name}</td>
-        <td>{movie.numberInStock}</td>
-        <td>{movie.dailyRentalRate}</td>
-        <td>
-          <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
-        </td>
-        <td>
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={() => this.handleDelete(movie)}
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
-    ));
   }
 }
 
